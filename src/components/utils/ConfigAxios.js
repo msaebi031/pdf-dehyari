9815aabefa544da09b460251b95a94acfe4e1fd7
msaebi031 <m.saebi9@gmail.com>
2023-03-31 02:44:04 +0430

    okut

--- src/components/utils/ConfigAxios.js
+++ src/components/utils/ConfigAxios.js
@@ -2,7 +2,7 @@ import axios from "axios";
 
 // Set config defaults when creating the instance
 const instance = axios.create({
-  baseURL: "http://localhost:3000",
+  baseURL: "https://pdf-dehyari.vercel.app",
   headers: {
     Authorization: "6LfvH-YZAAAAAMSSiVglqs0tE0mXr1sIdFgScwM-",
     "Content-Type": "application/json",
